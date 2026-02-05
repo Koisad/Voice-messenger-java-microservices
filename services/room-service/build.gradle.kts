f9578d40727d3fc04519d4c70bf4ce1e37d599dc
Koisad <patrykpaczkow@gmail.com>
2026-02-05 21:51:36 +0100

    feat: add notifications

--- services/room-service/build.gradle.kts
+++ services/room-service/build.gradle.kts
@@ -1,10 +1,12 @@
 dependencies {
+    implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
