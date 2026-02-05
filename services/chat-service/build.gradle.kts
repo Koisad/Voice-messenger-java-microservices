2cc5a57ccea394853a02a7cae6a1251c018fda2c
Koisad <patrykpaczkow@gmail.com>
2026-02-05 20:23:55 +0100

    feat(chat): changed simplebroker to rabbitmq

--- services/chat-service/build.gradle.kts
+++ services/chat-service/build.gradle.kts
@@ -3,6 +3,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation ("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("io.projectreactor.netty:reactor-netty")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
