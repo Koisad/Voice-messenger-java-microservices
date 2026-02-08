496be93f7073f9bea2dac192ea396a5e6420f305
Koisad <patrykpaczkow@gmail.com>
2026-02-08 23:54:08 +0100

    feat: add jackson2json converter

--- services/chat-service/src/main/java/com/voicecommunicator/chat/config/AmqpConfig.java
+++ services/chat-service/src/main/java/com/voicecommunicator/chat/config/AmqpConfig.java
@@ -1,5 +1,7 @@
 package com.voicecommunicator.chat.config;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
@@ -14,6 +16,10 @@ public class AmqpConfig {
     @Bean
     public Queue resultQueue() {
         return new Queue("text.result", true);
+    }
 
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
